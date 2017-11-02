266ec27c5b3e5663890bfe75f6e4f99810f733e9
tobinjo <tobinjo@umich.edu>
2017-11-02 14:11:09 -0400

    Added basic class files for our datastructures.

---
+++ app/src/main/java/com/example/johntobin/thescheduleviz/Event.java
@@ -0,0 +1,8 @@
+package com.example.johntobin.thescheduleviz;
+
+/**
+ * Created by johntobin on 11/2/17.
+ */
+
+public class Event {
+}
---
+++ app/src/main/java/com/example/johntobin/thescheduleviz/User.java
@@ -0,0 +1,9 @@
+package com.example.johntobin.thescheduleviz;
+
+/**
+ * Created by johntobin on 11/2/17.
+ */
+
+public class User {
+
+}
