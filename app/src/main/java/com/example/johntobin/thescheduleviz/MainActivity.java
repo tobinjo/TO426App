1511498659eea184d6a0d9bc616fd3a7ea68c904
tobinjo <tobinjo@umich.edu>
2017-11-08 12:34:14 -0500

    Copied in screenshot code.

--- app/src/main/java/com/example/johntobin/thescheduleviz/MainActivity.java
+++ app/src/main/java/com/example/johntobin/thescheduleviz/MainActivity.java
@@ -5,6 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import java.io.File;
+import java.io.FileOutputStream;
+import android.net.Uri;
+import android.widget.Toast;
+import android.content.ActivityNotFoundException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,13 +34,62 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        takeScreenshot = (Button) findViewById(R.id.goToCalendarTest);
+        takeScreenshot = (Button) findViewById(R.id.screenshotTest);
         takeScreenshot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
+                Bitmap ss = getScreenShot(rootView);
+                store(ss, "Testfile");
             }
         });
 
+    }
+
+    public static Bitmap getScreenShot(View view) {
+        View screenView = view.getRootView();
+        screenView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
+        screenView.setDrawingCacheEnabled(false);
+        return bitmap;
+    }
+
+    public void store(Bitmap bm, String fileName){
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            final String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Screenshots";
+            File dir = new File(dirPath);
+            if(!dir.exists())
+                dir.mkdirs();
+            File file = new File(dirPath, fileName);
+            try {
+                FileOutputStream fOut = new FileOutputStream(file);
+                bm.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+                fOut.flush();
+                fOut.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            Toast.makeText(getApplicationContext(), Environment.getExternalStorageState(), Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void shareImage(File file){
+        Uri uri = Uri.fromFile(file);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("image/*");
+
+        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+        try {
+            startActivity(Intent.createChooser(intent, "Share Screenshot"));
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this.getApplicationContext(), "No App Available", Toast.LENGTH_SHORT).show();
+        }
     }
 }
