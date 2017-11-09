d572e55f5912809685da28046ef21b811a4006df
tobinjo <tobinjo@umich.edu>
2017-11-09 13:50:42 -0500

    Added blank activities to the repository.

---
+++ app/src/main/java/com/example/johntobin/thescheduleviz/EventAddActivity.java
@@ -0,0 +1,13 @@
+package com.example.johntobin.thescheduleviz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_add);
+    }
+}
---
+++ app/src/main/java/com/example/johntobin/thescheduleviz/NewUserRegistration.java
@@ -0,0 +1,13 @@
+package com.example.johntobin.thescheduleviz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewUserRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_registration);
+    }
+}
