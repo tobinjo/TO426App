539326908291fe470519438fa736c60bed8f445e
jilliansmith <jillysm@umich.edu>
2017-11-12 15:17:58 -0500

    created button for Confirmation Activity, error showing because id has not been picked up yet

--- app/src/main/java/com/example/johntobin/thescheduleviz/ConfirmationActivity.java
+++ app/src/main/java/com/example/johntobin/thescheduleviz/ConfirmationActivity.java
@@ -2,12 +2,20 @@ package com.example.johntobin.thescheduleviz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ConfirmationActivity extends AppCompatActivity {
+
+    Button buttonConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        buttonConfirm = (Button) findViewById(R.id.buttonConfirm);
+
+
+
     }
 }
