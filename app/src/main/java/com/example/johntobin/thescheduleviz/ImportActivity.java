b3f34dd312dddd892e59445fa7382eae1f41e1c5
jasminepalakurthi <jpalakurthi@gmail.com>
2017-12-09 21:39:25 -0500

    #back button on import page

--- app/src/main/java/com/example/johntobin/thescheduleviz/ImportActivity.java
+++ app/src/main/java/com/example/johntobin/thescheduleviz/ImportActivity.java
@@ -1,7 +1,9 @@
 package com.example.johntobin.thescheduleviz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class ImportActivity extends AppCompatActivity {
@@ -9,6 +11,8 @@ public class ImportActivity extends AppCompatActivity {
     Button buttonImportGoogleCalendar;
     Button buttonImportFromICal;
     Button buttonManuallyImport;
+
+    Button buttonBackFromImport; //jasmine added back button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,23 @@ public class ImportActivity extends AppCompatActivity {
         buttonImportFromICal = (Button) findViewById(R.id.buttonImportFromICal);
         buttonManuallyImport = (Button) findViewById(R.id.buttonManuallyImport);
 
+        buttonBackFromImport = (Button) findViewById(R.id.buttonBackFromImport); //jasmine added back button
+
+        buttonBackFromImport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+
+
+        });
+    }
+    private void launchActivity() {
+
+        Intent goToSplash = new Intent(this, SplashPage.class);
+        startActivity(goToSplash);
     }
 }
+
+
+
